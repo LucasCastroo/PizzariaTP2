4ff3816589a03a90626cf1f153ea29e95a06388e
LucasCastroo <lucascastropmw@gmail.com>
2024-04-02 16:06:08 -0300

    Padronização do CRUD Cupom

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { Routes } from '@angular/router';
-import {CupomListComponent} from "./components/cupom-list/cupom-list.component";
 import {LoginFormComponent} from "./components/login-form/login-form.component";
 import {funcionarioResolver} from "./components/funcionario/conta/resolver/funcionario.resolver";
 import {ContaFuncionarioComponent} from "./components/funcionario/conta/conta-funcionario.component";
 import {clienteResolver} from "./components/cliente/conta/resolver/cliente-resolver";
 import {ContaClienteComponent} from "./components/cliente/conta/conta-cliente.component";
+import {CupomComponent} from "./components/cupom/cupom.component";
 
 export const routes: Routes = [
-  {path: "login", component: LoginFormComponent, title: "Login"},
-  {path: "login-admin", component: LoginFormComponent, title: "Login"},
-  {path: "cupons", component: CupomListComponent, title: "Lista de Cupons"},
+  { path: "login", component: LoginFormComponent, title: "Login"},
+  { path: "login-admin", component: LoginFormComponent, title: "Login"},
+  { path: "cupons", component: CupomComponent, title: "Cupons"},
   { path: 'contas-cliente', component: ContaClienteComponent, title: 'Contas' },
   { path: 'contas-cliente/edit/:id', component: ContaClienteComponent, resolve: {cliente: clienteResolver}},
 
--- frontend/src/app/components/login-form/login-form.component.ts
+++ frontend/src/app/components/login-form/login-form.component.ts
@@ -1,24 +1,27 @@
 import { Component } from '@angular/core';
-import {MatCard, MatCardContent, MatCardModule} from "@angular/material/card";
-import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
-import {MatError, MatFormField, MatFormFieldModule} from "@angular/material/form-field";
-import {MatInput, MatInputModule} from "@angular/material/input";
+import {MatCardModule} from "@angular/material/card";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
 import {NgIf} from "@angular/common";
 import {MatButtonModule} from "@angular/material/button";
 import {AuthService} from "../../services/auth.service";
 import {ActivatedRoute} from "@angular/router";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
   imports: [
     NgIf, ReactiveFormsModule, MatFormFieldModule,
-    MatInputModule, MatButtonModule, MatCardModule
+    MatInputModule, MatButtonModule, MatCardModule,
+    MatIconModule
   ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
 export class LoginFormComponent {
+  hide = true;
   formGroup: FormGroup = new FormGroup(
     {
       email: new FormControl('', [Validators.required, Validators.email]),
