28dab1595a88d2760707009acc7bf613d290f46a
Jônatas Pereira <jonataspereira@unitins.br>
2024-04-05 03:37:54 -0300

    Consertar as rotas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,21 +1,16 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {LoginComponent} from "./components/login/login.component";
 import {FuncionarioContaComponent} from "./components/funcionario/conta/funcionario-conta.component";
 import {ClienteContaComponent} from "./components/cliente/conta/cliente-conta.component";
 import {CupomComponent} from "./components/cupom/cupom.component";
 import {PizzaListComponent} from "./components/pizza/pizza-list.component";
 import {IngredienteListComponent} from "./components/ingrediente/ingrediente-list.component";
+import {BebidaListComponent} from "./components/bebida/bebida-list.component";
 
-export const routes: Routes = [
-  { path: "login", component: LoginComponent, title: "Login"},
-
-  { path: "cupons", component: CupomComponent, title: "Cupons"},
-
-  { path: 'contas-cliente', component: ClienteContaComponent, title: 'Contas Clientes' },
-
-  { path: 'contas-funcionario', component: FuncionarioContaComponent, title: 'Contas Colaboradores' },
-
+export const routes: Routes = [{path: "login", component: LoginComponent, title: "Login"},
+  {path: "cupons", component: CupomComponent, title: "Cupons"},
+  {path: 'contas-cliente', component: ClienteContaComponent, title: 'Contas Clientes'},
+  {path: 'contas-funcionario', component: FuncionarioContaComponent, title: 'Contas Colaboradores'},
   {path: "pizza", component: PizzaListComponent, title: "Pizzas"},
-
-  {path: "ingrediente", component: IngredienteListComponent, title: "Ingredientes"}
-];
+  {path: "ingrediente", component: IngredienteListComponent, title: "Ingredientes"},
+  {path: "bebida", component: BebidaListComponent, title: "Bebidas"}];
