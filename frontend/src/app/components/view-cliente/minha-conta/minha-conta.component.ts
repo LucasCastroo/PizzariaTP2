e97ac233c2df63b72d559a14711bcba0339c7b47
LucasCastroo <lucascastropmw@gmail.com>
2024-06-02 16:20:16 -0300

    Tela de Meus Dados do cliente

--- frontend/src/app/components/view-cliente/minha-conta/minha-conta.component.ts
+++ frontend/src/app/components/view-cliente/minha-conta/minha-conta.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from "../../template/cliente/header/header.component";
 import {MatIcon} from "@angular/material/icon";
 import {MatInputModule} from "@angular/material/input";
-import {NgOptimizedImage} from "@angular/common";
+import {NgForOf, NgOptimizedImage} from "@angular/common";
 import {Usuario} from "../../../models/usuario";
 import {MatButton, MatFabButton, MatIconButton, MatMiniFabButton} from "@angular/material/button";
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatButtonToggle} from "@angular/material/button-toggle";
+import {FormsModule} from "@angular/forms";
+import {ChangePasswordDialogComponent} from "./change-password-dialog/change-password-dialog.component";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-minha-conta',
@@ -21,7 +24,9 @@ import {MatButtonToggle} from "@angular/material/button-toggle";
     MatExpansionModule,
     MatButtonToggle,
     MatButton,
-    MatIconButton
+    MatIconButton,
+    FormsModule,
+    NgForOf
   ],
   templateUrl: './minha-conta.component.html',
   styleUrl: './minha-conta.component.css'
@@ -29,5 +34,42 @@ import {MatButtonToggle} from "@angular/material/button-toggle";
 export class MinhaContaComponent {
   usuarioLogado: Usuario | undefined;
 
+  constructor(protected dialog: MatDialog) {
+  }
 
+  addresses: any[] = [
+    {
+      alias: 'Apelido do Endereço',
+      description: 'Breve descrição do endereço',
+      rua: '',
+      numero: '',
+      cep: '',
+      bairro: '',
+      complemento: ''
+    }
+  ];
+
+  addAddress() {
+    this.addresses.push({
+      alias: 'Novo endereço',
+      description: '',
+      rua: '',
+      numero: '',
+      cep: '',
+      bairro: '',
+      complemento: ''
+    });
+  }
+
+  removeAddress(index: number) {
+    this.addresses.splice(index, 1);
+  }
+
+  saveAddress(index: number) {
+    // Logic to save address
+    console.log('Saving address at index', index);
+  }
+
+  protected readonly open = open;
+  protected readonly DialogChangePasswordComponent = ChangePasswordDialogComponent;
 }
