a0d74537b40c29b76a550eea7481eb77e7e64b8b
LucasCastroo <lucascastropmw@gmail.com>
2024-06-02 14:04:10 -0300

    Continuação da tela de dados da conta do cliente

--- frontend/src/app/components/view-cliente/minha-conta/minha-conta.component.ts
+++ frontend/src/app/components/view-cliente/minha-conta/minha-conta.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from "../../template/cliente/header/header.component";
 import {MatIcon} from "@angular/material/icon";
 import {MatInputModule} from "@angular/material/input";
+import {NgOptimizedImage} from "@angular/common";
+import {Usuario} from "../../../models/usuario";
+import {MatButton, MatFabButton, MatIconButton, MatMiniFabButton} from "@angular/material/button";
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatButtonToggle} from "@angular/material/button-toggle";
 
 @Component({
   selector: 'app-minha-conta',
@@ -9,11 +14,20 @@ import {MatInputModule} from "@angular/material/input";
   imports: [
     HeaderComponent,
     MatIcon,
-    MatInputModule
+    MatInputModule,
+    NgOptimizedImage,
+    MatFabButton,
+    MatMiniFabButton,
+    MatExpansionModule,
+    MatButtonToggle,
+    MatButton,
+    MatIconButton
   ],
   templateUrl: './minha-conta.component.html',
   styleUrl: './minha-conta.component.css'
 })
 export class MinhaContaComponent {
+  usuarioLogado: Usuario | undefined;
+
 
 }
